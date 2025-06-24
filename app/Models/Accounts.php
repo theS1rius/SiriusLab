77a216c4d01eaf663b6d33bed05955417efd908c
Sirius <thesirius0927@gmail.com>
2025-06-24 17:05:33 +0800

    update Model Accounts

--- app/Models/Accounts.php
+++ app/Models/Accounts.php
@@ -7,6 +7,34 @@ use Illuminate\Database\Eloquent\Model;
 
 class Accounts extends Model
 {
-    /** @use HasFactory<\Database\Factories\AccountsFactory> */
     use HasFactory;
+
+    protected $table = 'Accounts';
+
+    protected $primaryKey = 'Id';
+
+    protected $fillable = [
+        'Name',
+        'Email',
+        'Password',
+        'EmailVerifiedAt',
+        'FullName',
+        'Gender',
+        'IsEmailVerified',
+        'Status',
+        'Role',
+        'LastLoginAt',
+        'LastLoginIp',
+        'LoginAttempts',
+        'Photo',
+        'Language',
+        'SubscribeNewsletter',
+    ];
+
+    /**
+     * 隱藏的屬性
+     */
+    protected $hidden = [
+        'Password',
+    ];
 }
